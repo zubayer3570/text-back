5447be0e22f7243274d1fe480124862a84f0c6ec
zubayer <zubayer3570@gmail.com>
2022-04-30 05:30:32 +0600

    '3'

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { MongoClient } = require('mongodb')
 app.use(cors())
 app.use(express.json())
 //port
-const port = 5000
+const port = process.env.PORT || 5000
 //mongo
 const uri = "mongodb+srv://database-user-1:databaseofzubayer@cluster0.1f3iy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const client = new MongoClient(uri)
